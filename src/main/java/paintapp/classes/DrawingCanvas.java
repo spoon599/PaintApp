1744e30296291c30655bb0996acee1c471104c7b
spoon599 <nolanmatson5253@gmail.com>
2026-09-23 03:43:08 -0500

    Separated drawing tasks into new DrawingCanvas class

---
+++ src/main/java/paintapp/classes/DrawingCanvas.java
@@ -0,0 +1,51 @@
+package paintapp.classes;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+public class DrawingCanvas extends CustomCanvas {
+
+    private double lastX;
+    private double lastY;
+
+    public DrawingCanvas() {}
+
+    /**
+     * Sets up the canvas for drawing.
+     */
+    public void setupDrawing() {
+        GraphicsContext graphics = this.getGraphicsContext2D();
+        graphics.setStroke(Color.BLACK);
+        graphics.setLineWidth(3);
+
+        this.setOnMousePressed(event -> {
+            lastX = event.getX();
+            lastY = event.getY();
+        });
+
+        this.setOnMouseDragged(event -> {
+            double currentX = event.getX();
+            double currentY = event.getY();
+
+            graphics.strokeLine(lastX, lastY, currentX, currentY);
+
+            lastX = currentX;
+            lastY = currentY;
+        });
+    }
+
+    /**
+     * Removes all drawings from the current canvas.
+     */
+    public void clearDrawings() {
+        GraphicsContext graphics = this.getGraphicsContext2D();
+        graphics.clearRect(
+            0, 
+            0, 
+            this.getWidth(), 
+            this.getHeight()
+        );
+        graphics.restore();
+    }
+    
+}
--- src/main/java/paintapp/controllers/ImageController.java
+++ src/main/java/paintapp/controllers/ImageController.java
@@ -4,22 +4,17 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
 
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
-import paintapp.classes.CustomCanvas;
 import paintapp.classes.CustomScrollPane;
 import paintapp.classes.CustomStackPane;
+import paintapp.classes.DrawingCanvas;
 
 public class ImageController {
 
     private final ImageView imageView;
-    private final CustomCanvas canvas; // drawing sheet
+    private final DrawingCanvas drawingCanvas; // drawing sheet
     private final CustomStackPane stackPane; // snapshot container (image + drawing sheet)
     private final CustomStackPane workspace; // global allignment pane
     private final CustomScrollPane scrollPane; // scroll movement, top of chain
-
-    private double lastX;
-    private double lastY;
     
     /**
      * Creates a new ImageController with a default ImageView.
@@ -28,13 +23,13 @@ public class ImageController {
         imageView = new ImageView();
         imageView.setPreserveRatio(true); // preserve image's aspect ratio when setting a new image
 
-        canvas = new CustomCanvas();
+        drawingCanvas = new DrawingCanvas();
         
         // actual image stack
         stackPane = new CustomStackPane();
         stackPane.getChildren().addAll(
             imageView, // add this first as our bottom layer
-            canvas // invisible top layer for drawing
+            drawingCanvas // invisible top layer for drawing
         );
 
         // used to center the stackPane
@@ -44,96 +39,43 @@ public class ImageController {
         scrollPane = new CustomScrollPane(workspace);
         scrollPane.setFitToDimensions(true);
 
-        setupDrawing();
+        drawingCanvas.setupDrawing();
     }
     
-    /**
-     * Returns the current image being displayed within the ImageView.
-     */
     public Image getImage() {
         return imageView.getImage();
     }
-
-    /**
-     * Returns the current ImageView.
-     */
     public ImageView getImageView() {
         return imageView;
     }
-
-    /**
-     * Returns the pane containing the image and drawing canvas.
-     */
     public CustomStackPane getStackPane() {
         return stackPane;
     }
-
-    /**
-     * Returns the workspace stackpane.
-     */
     public CustomStackPane getWorkspace() {
         return workspace;
     }
-
     public CustomScrollPane getScrollPane() {
         return scrollPane;
     }
-
+    
     /**
      * Sets the image displayed in the controller's ImageView.
      * @param image The image to display
      */
     public void setImage(Image image) {
         imageView.setImage(image); // apply image (aspect ratio preserved by default)
-        canvas.sizeTo(image);
+        drawingCanvas.sizeTo(image);
         stackPane.sizeTo(image);
-        cleanCanvas(); // clear the canvas when a new image is set
-    }
-
-    /**
-     * Sets up the canvas for drawing on top of the image.
-     */
-    private void setupDrawing() {
-        GraphicsContext graphics = canvas.getGraphicsContext2D();
-        graphics.setStroke(Color.BLACK);
-        graphics.setLineWidth(3);
-
-        canvas.setOnMousePressed(event -> {
-            lastX = event.getX();
-            lastY = event.getY();
-        });
-
-        canvas.setOnMouseDragged(event -> {
-            double currentX = event.getX();
-            double currentY = event.getY();
-
-            graphics.strokeLine(lastX, lastY, currentX, currentY);
-
-            lastX = currentX;
-            lastY = currentY;
-        });
-    }
-
-    /**
-     * Removes all drawings from the current canvas.
-     */
-    private void cleanCanvas() {
-        GraphicsContext graphics = canvas.getGraphicsContext2D();
-        graphics.clearRect(
-            0, 
-            0, 
-            canvas.getWidth(), 
-            canvas.getHeight()
-        );
-        graphics.restore();
+        
+        drawingCanvas.clearDrawings(); // clear the canvas when a new image is set
     }
 
     /**
      * Returns a snapshot of the current image and drawings as a new image.
      */
     public Image getModifiedImage() {
-        int width = (int) canvas.getWidth();
-        int height = (int) canvas.getHeight();
+        int width = (int) drawingCanvas.getWidth();
+        int height = (int) drawingCanvas.getHeight();
 
         if (width <= 0 || height <= 0) {
             throw new IllegalStateException("Canvas dimensions must be positive to create a modified image.");
